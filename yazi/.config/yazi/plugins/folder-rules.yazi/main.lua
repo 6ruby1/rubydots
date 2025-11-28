d7d4a0da4cb2e6e4d962863148fa25805c57f757
6ruby1 <ruby@rubygem.dev>
2025-11-28 11:49:26 +1100

    feat(yazi): update auto mtime sorting for given dirs

--- yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
+++ yazi/.config/yazi/plugins/folder-rules.yazi/main.lua
@@ -1,11 +1,28 @@
+-- INFO: Not managed by package manager!
+
 local function setup()
+	-- list of trailing-path fragments to sort by mtime
+	local mtime_suffixes = {
+		"Downloads",
+		"Trash/files",
+		"tmp",
+		-- add more suffixes here
+	}
+
 	ps.sub("cd", function()
-		local cwd = cx.active.current.cwd
-		if cwd:ends_with("Downloads") then
-			ya.emit("sort", { "mtime", reverse = true, dir_first = false })
-		else
-			ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
+		local cwd = cx and cx.active and cx.active.current and cx.active.current.cwd
+		if not cwd then
+			return
 		end
+
+		for _, suffix in ipairs(mtime_suffixes) do
+			if cwd:ends_with(suffix) then
+				ya.emit("sort", { "mtime", reverse = true, dir_first = false })
+				return
+			end
+		end
+
+		ya.emit("sort", { "alphabetical", reverse = false, dir_first = true })
 	end)
 end
 
