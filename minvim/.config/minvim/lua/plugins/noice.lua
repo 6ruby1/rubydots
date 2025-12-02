0f15bd918400e0efce8946000aed6538d2b99309
6ruby1 <ruby@rubygem.dev>
2025-12-02 16:44:34 +1100

    feat(nvim): add noice.nvim for improved cmdline

---
+++ minvim/.config/minvim/lua/plugins/noice.lua
@@ -0,0 +1,47 @@
+---@type LazySpec
+return {
+	{
+		"folke/noice.nvim",
+		dependencies = {
+			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+			"MunifTanjim/nui.nvim",
+			-- OPTIONAL:
+			--   `nvim-notify` is only needed, if you want to use the notification view.
+			--   If not available, we use `mini` as the fallback
+			-- "rcarriga/nvim-notify",
+		},
+		event = "VeryLazy",
+		opts = {},
+		config = function()
+			require("noice").setup({
+				presets = {
+					-- you can enable a preset by setting it to true, or a table that will override the preset config
+					-- you can also add custom presets that you can enable/disable with enabled=true
+					-- bottom_search = false, -- use a classic bottom cmdline for search
+					-- command_palette = true, -- position the cmdline and popupmenu together
+					long_message_to_split = true, -- long messages will be sent to a split
+					inc_rename = true, -- enables an input dialog for inc-rename.nvim
+				},
+				views = {
+					cmdline_popup = {
+						position = {
+							row = -2,
+							col = "0%",
+						},
+						size = {
+							width = "100%",
+							height = "auto",
+						},
+						border = {
+							style = "none",
+							padding = { 0, 4 },
+						},
+						win_options = {
+							winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+						},
+					},
+				},
+			})
+		end,
+	},
+}
