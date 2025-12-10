a467a4f32ccdaae59b1e214c4bb7526de1fb89dd
6ruby1 <ruby@rubygem.dev>
2025-12-10 14:41:07 +1100

    feat(nvim): add nvim-lint

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -22,48 +22,4 @@ return {
 			},
 		},
 	},
-	-- {
-	-- 	"shellRaining/hlchunk.nvim",
-	-- 	event = { "BufReadPre", "BufNewFile" },
-	-- 	config = function()
-	-- 		require("hlchunk").setup({
-	-- 			chunk = {
-	-- 				enable = true,
-	-- 				priority = 15,
-	-- 				style = {
-	-- 					{ fg = "#f7c67f" },
-	-- 					-- 		{ fg = "#f16c75" },
-	-- 				},
-	-- 				use_treesitter = true,
-	-- 				-- 	chars = {
-	-- 				-- 		horizontal_line = "─",
-	-- 				-- 		vertical_line = "│",
-	-- 				-- 		left_top = "╭",
-	-- 				-- 		left_bottom = "╰",
-	-- 				-- 		right_arrow = ">",
-	-- 				-- 	},
-	-- 				textobject = "oc",
-	-- 				-- 	max_file_size = 1024 * 1024,
-	-- 				-- 	error_sign = true,
-	-- 				-- 	-- animation related
-	-- 				-- 	duration = 100,
-	-- 				-- 	delay = 200,
-	-- 				-- },
-	-- 				-- indent = {
-	-- 				-- 	enable = true,
-	-- 				-- 	priority = 10,
-	-- 				-- 	style = { vim.fn.synIDattr(vim.fn.synIDtrans(vim.fn.hlID("Whitespace")), "fg", "gui") },
-	-- 				-- 	use_treesitter = false,
-	-- 				-- 	chars = {
-	-- 				-- 		"│",
-	-- 				-- 		"¦",
-	-- 				-- 		"┆",
-	-- 				-- 		"┊",
-	-- 				-- 	},
-	-- 				-- 	ahead_lines = 5,
-	-- 				-- 	delay = 100,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
 }
